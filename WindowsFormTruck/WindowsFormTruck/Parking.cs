b971a5c68f9c08256e3fb6eae3be7b152ee44041
unknown <varya.yurtanowa@yandex.ru>
2018-11-29 23:09:40 +0300

    lab4_C#

--- WindowsFormTruck/WindowsFormTruck/Parking.cs
+++ WindowsFormTruck/WindowsFormTruck/Parking.cs
@@ -16,7 +16,11 @@ namespace WindowsFormTruck
         /// <summary>
         /// Массив объектов, которые храним
         /// </summary>
-        private T[] _places;
+        private Dictionary<int, T> _places;
+        /// <summary>
+        /// Максимальное количество мест на парковке
+        /// </summary>
+        private int _maxCount;
         /// <summary>
         /// Ширина окна отрисовки
         /// </summary>
@@ -41,13 +45,10 @@ namespace WindowsFormTruck
         /// <param name="pictureHeight">Рамзер парковки - высота</param>
         public Parking(int sizes, int pictureWidth, int pictureHeight)
         {
-            _places = new T[sizes];
+            _maxCount = sizes;
+            _places = new Dictionary<int, T>();
             PictureWidth = pictureWidth;
             PictureHeight = pictureHeight;
-            for (int i = 0; i < _places.Length; i++)
-            {
-                _places[i] = null;
-            }
         }
         /// <summary>
         /// Перегрузка оператора сложения
@@ -58,11 +59,15 @@ namespace WindowsFormTruck
         /// <returns></returns>
         public static int operator +(Parking<T> p, T car)
         {
-            for (int i = 0; i < p._places.Length; i++)
+            if (p._places.Count == p._maxCount)
+            {
+                return -1;
+            }
+            for (int i = 0; i < p._maxCount; i++)
             {
                 if (p.CheckFreePlace(i))
                 {
-                    p._places[i] = car;
+                    p._places.Add(i, car);
                     p._places[i].SetPosition(5 + i / 5 * p._placeSizeWidth + 5,
                     i % 5 * p._placeSizeHeight + 15, p.PictureWidth, p.PictureHeight);
                     return i;
@@ -79,14 +84,10 @@ namespace WindowsFormTruck
         /// <returns></returns>
         public static T operator -(Parking<T> p, int index)
         {
-            if (index < 0 || index > p._places.Length)
-            {
-                return null;
-            }
             if (!p.CheckFreePlace(index))
             {
                 T car = p._places[index];
-                p._places[index] = null;
+                p._places.Remove(index);
                 return car;
             }
             return null;
@@ -98,7 +99,7 @@ namespace WindowsFormTruck
         /// <returns></returns>
         private bool CheckFreePlace(int index)
         {
-            return _places[index] == null;
+            return !_places.ContainsKey(index);
         }
         /// <summary>
         /// Метод отрисовки парковки
@@ -107,12 +108,10 @@ namespace WindowsFormTruck
         public void Draw(Graphics g)
         {
             DrawMarking(g);
-            for (int i = 0; i < _places.Length; i++)
+            var keys = _places.Keys.ToList();
+            for (int i = 0; i < keys.Count; i++)
             {
-                if (!CheckFreePlace(i))
-                {//если место не пустое
-                    _places[i].DrawCar(g);
-                }
+                _places[keys[i]].DrawCar(g);
             }
         }
         /// <summary>
@@ -123,8 +122,8 @@ namespace WindowsFormTruck
         {
             Pen pen = new Pen(Color.Black, 3);
             //границы праковки
-            g.DrawRectangle(pen, 0, 0, (_places.Length / 5) * _placeSizeWidth, 480);
-            for (int i = 0; i < _places.Length / 5; i++)
+            g.DrawRectangle(pen, 0, 0, (_maxCount / 5) * _placeSizeWidth, 480);
+            for (int i = 0; i < _maxCount / 5; i++)
             {//отрисовываем, по 5 мест на линии
                 for (int j = 0; j < 6; ++j)
                 {//линия рамзетки места
